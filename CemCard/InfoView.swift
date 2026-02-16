1dc8389dc34f7d7b5374aae54f12ed676e59ecc1
Dev-MuTTiNeeR <cemakkaya1312@gmail.com>
2026-02-16 17:32:31 -0300

    Created test SwiftUI Project

---
+++ CemCard/InfoView.swift
@@ -0,0 +1,18 @@
+//
+//  InfoView.swift
+//  CemCard
+//
+//  Created by Cem Akkaya on 16/02/26.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoView()
+}
