39bdff3c0411f12ff0ee174b9f7ad493792c233c
Dev-MuTTiNeeR <cemakkaya1312@gmail.com>
2026-02-16 17:10:42 -0300

    Initial Commit

---
+++ CemCard/CemCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CemCardApp.swift
+//  CemCard
+//
+//  Created by Cem Akkaya on 16/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CemCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
